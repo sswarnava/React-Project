f46690011029e7f83654ab0dea1ebc0d5ecbb283
Swarnava <swarnavamudi22@gmail.com>
2023-07-31 22:27:46 +0530

    Almost done the a qna app using chatgpt api

---
+++ chatgptProject/client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ChatGpt from './components/ChatGpt';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatGpt />
+    </div>
+  );
+}
+
+export default App;
