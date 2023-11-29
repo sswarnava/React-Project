9b7aae375c3cd90f590ccda5bde869a2c48a39af
Swarnava <swarnavamudi22@gmail.com>
2023-11-30 00:44:26 +0530

    currency project has started and custom hook is done

---
+++ currencyConverter-Project/src/hooks/useCurrencyinfo.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export default function useCurrencyInfo(currency) {
+    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]))
+        console.log(data)
+    }, [currency])
+
+    console.log(data)
+    return data
+}
