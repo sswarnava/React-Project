bd2c77d55e12f7e900d424b72930edeb176e9d18
Swarnava <swarnavamudi22@gmail.com>
2023-12-06 23:52:47 +0530

    small changes and bug fixed

--- randomColorGenarate-project/src/components/RandomColor.jsx
+++ randomColorGenarate-project/src/components/RandomColor.jsx
@@ -10,7 +10,7 @@ export default function RandomColor() {
             style={{ height: '100vh', background: color }}
             className="flex justify-center">
 
-                <h3 className="absolute top-60 font-extrabold text-5xl text-white border-2 p-5 rounded-xl bg-slate-800">{color}</h3>
+            <h3 className="absolute top-60 font-extrabold text-5xl text-white border-2 p-5 rounded-xl bg-slate-800">{color}</h3>
 
             {/* Random color generator button*/}
             <h4 className="absolute bottom-60 text-white font-semibold underline mb-6">Select random color using this Button</h4>
@@ -22,50 +22,52 @@ export default function RandomColor() {
             </button>
 
             {/* Fixed color button's group */}
-            <h4 className="absolute bottom-20 text-white font-semibold underline mb-2">Select Fixed color using this Navbar</h4>
-            <div className="flex justify-center h-14 gap-10 px-10 rounded absolute bottom-5 bg-gray-100 ">
+            <h4 className="absolute bottom-28 sm:bottom-20 text-white font-semibold underline mb-2">Select Fixed color using this Navbar</h4>
+            <div className="grid grid-cols-3 sm:flex sm:flex-row justify-center h-auto sm:h-14 gap-3 gap-x-10  sm:gap-10 px-2 sm:px-10 rounded absolute bottom-5 bg-gray-100">
                 {/* black button */}
                 <button
-                    className="font-bold text-black"
+                    className="font-bold text-black my-2 sm:my-0 sm:mr-2"
                     onClick={() => { setColor('#0a0e07') }}>
                     Black
                 </button>
 
                 {/* red button */}
                 <button
-                    className="font-bold text-red-500"
+                    className="font-bold text-red-500 my-2 sm:my-0 sm:mr-2"
                     onClick={() => { setColor('#C42424') }}>
                     Red
                 </button>
 
                 {/* blue button */}
                 <button
-                    className="font-bold text-blue-500"
+                    className="font-bold text-blue-500 my-2 sm:my-0 sm:mr-2"
                     onClick={() => { setColor('#24a0c4') }}>
                     Blue
                 </button>
 
                 {/* green button */}
                 <button
-                    className="font-bold text-green-500"
+                    className="font-bold text-green-500 my-2 sm:my-0 sm:mr-2"
                     onClick={() => { setColor('#4bc424') }}>
                     Green
                 </button>
 
                 {/* yellow button */}
                 <button
-                    className="font-bold text-yellow-500"
+                    className="font-bold text-yellow-500 my-2 sm:my-0 sm:mr-2"
                     onClick={() => { setColor('#F5c213') }}>
                     Yellow
                 </button>
 
-                {/* yellow button */}
+                {/* white button */}
                 <button
-                    className="font-bold text-gray-400"
+                    className="font-bold text-gray-400 my-2 sm:my-0"
                     onClick={() => { setColor('#Eeeaf5') }}>
                     White
                 </button>
             </div>
+
+
         </div>
     )
 }
