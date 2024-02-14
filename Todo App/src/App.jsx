c4e5cabf11b1aa086e8aa085c657ab2a7b69a5c8
Swarnava <swarnavamudi22@gmail.com>
2024-02-14 23:57:20 +0530

    Start css update

--- Todo App/src/App.jsx
+++ Todo App/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
 
   return (
     <TodoProvider value={{ todos, addTodo, deleteTodo, updateTodo, toggleComplete }}>
-      <div className="bg-[#341742] min-h-screen py-8">
+      <div className="bg-[#141414] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Hello Todos</h1>
 
